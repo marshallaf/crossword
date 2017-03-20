244cc33df79ff245b9ec3328b2ae793f8b0542bf
Andrew Marshall <marshall.a.f@gmail.com>
2017-03-20 08:58:49 -0500

    finished setting up webpack

---
+++ app/index.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+import App from './App';
+
+ReactDOM.render(
+  <App>Hello world!</App>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
+const path = require('path');
+
 module.exports = {
   devtool: 'source-map',
-  entry: [
-    '.app/index.js'
-  ],
+  entry: {
+    app: [
+      './app/index.js'
+    ],
+  },
   output: {
-    path: __dirname + '/build',
-    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
       {test: /\.css$/, loader: 'style-loader!css-loader!sass-loader'},
     ],
+  },
+  devServer: {
+    inline: true,
   },
 }
