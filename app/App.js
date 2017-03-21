bbfccd16e00ce360887102582e5a91794841226f
Andrew Marshall <marshall.a.f@gmail.com>
2017-03-20 21:27:43 -0500

    started Cell component

--- app/App.js
+++ app/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import './styles.scss';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>{this.props.children}</div>
+      <div><Cell solution='A'/><Cell solution='B'/></div>
     )
+  }
+}
+
+class Cell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: null,
+      correct: false,
+      active: false,
+    };
+    console.log(this.props);
+    this.solution = this.props.solution;
+
+    this.toggleActive = this.toggleActive.bind(this);
+  }
+
+  toggleActive() {
+    this.setState({active: !this.state.active});
+  }
+
+  render() {
+    let classList = `cell ${this.state.correct ? 'correct' : 'incorrect'} ${this.state.active ? 'active' : ''}`;
+    return (
+      <div 
+        className={classList}
+        onClick={this.toggleActive}>
+      </div>
+    );
   }
 }
