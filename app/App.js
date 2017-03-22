6e25783f9bf7ca11e6df8443e190c759d21ea3c9
Andrew Marshall <marshall.a.f@gmail.com>
2017-03-21 20:44:21 -0500

    added auto increment after typing letter

--- app/App.js
+++ app/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './styles.scss';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeIndex: -1,
+    }
+    this.dims = {
+      height: 5,
+      width: 5,
+    };
+    this.refDict = {};
+
+    this.setActive = this.setActive.bind(this);
+  }
+
+  setActive(index) {
+    if (index == this.state.activeIndex) {
+      this.setState({activeIndex: -1});
+    } else {
+      this.setState({activeIndex: index});
+    }
+    this.refDict[index].textInput.focus();
+  }
+
   render() {
     let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXY'.split('');
     return (
-      <div>
-        {letters.map((letter, index) => <Cell key={index} solution={letter}/>)}
+      <div className='board'>
+        {letters.map((letter, index) => {
+          return (
+            <Cell 
+              key={index} 
+              solution={letter}
+              index={index}
+              activeIndex={this.state.activeIndex}
+              setActive={this.setActive}
+              ref={cellObj => this.refDict[index] = cellObj}/>
+            )
+      })}
       </div>
     )
   }
@@ -23,17 +57,11 @@ class Cell extends React.Component {
     this.solution = this.props.solution;
 
     this.toggleActive = this.toggleActive.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
   toggleActive() {
-    this.setState({active: !this.state.active});
-  }
-
-  handleKeyPress(e) {
-    e.preventDefault();
-    console.log(e);
+    this.props.setActive(this.props.index);
   }
 
   handleChange(e) {
@@ -51,15 +79,20 @@ class Cell extends React.Component {
       input: input,
       correct: correct,
     });
+    this.props.setActive(this.props.index+1);
   }
 
   render() {
-    let classList = `cell ${this.state.correct ? 'correct' : 'incorrect'} ${this.state.active ? 'active' : ''}`;
+    let isActive = this.props.activeIndex == this.props.index;
+    let classList = `cell ${this.state.correct ? 'correct' : 'incorrect'} ${isActive ? 'active' : ''}`;
     return (
       <div 
         className={classList}
         onClick={this.toggleActive}>{this.state.input}
-        <input value={this.state.input} onChange={this.handleChange} />
+        <input 
+          value={this.state.input} 
+          onChange={this.handleChange} 
+          ref={input => this.textInput = input}/>
       </div>
     );
   }
