0f7d55acef7b5e09fb8adb0a0604666a0ef2841d
Andrew Marshall <marshall.a.f@gmail.com>
2017-04-05 08:25:46 -0500

    reconfigure build and dev-serv

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/assets/',
+    publicPath: '/serve/',
     filename: 'bundle.js',
   },
   module: {
